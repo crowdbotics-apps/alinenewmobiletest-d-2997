dbe2ab15ddfac24234d3ef36d5eadb8c42fedd40
Crowdbotics <team@crowdbotics.com>
2020-04-16 15:48:07 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -25,7 +25,6 @@ class CustomText(models.Model):
 
 class HomePage(models.Model):
     body = models.TextField()
-    another_field = models.BooleanField(null=True, blank=True,)
 
     @property
     def api(self):
